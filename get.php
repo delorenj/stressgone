ef5d9dd5135dc70b973cfd7987be18e4bd481eb7
Jarad DeLorenzo (prod) <jaradd@gmail.com>
2022-01-30 10:36:59 -0700

    Initial prod commit

---
+++ get.php
@@ -0,0 +1,95 @@
+<html>
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-MPWPRK0XY9"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-MPWPRK0XY9');
+	</script>
+<center>
+	<body bgcolor="#E0FFFF">
+
+<BR>
+	
+	
+
+<center>
+
+	<table>
+
+		<tr>
+
+			<td> <img src="WSK Cover 2.png" height="300" border="1"><br></td>
+
+ <TD><font face="lucida bright" size="7" color="white">...</font><br></td></B>
+
+				 <TD><font face="lucida bright" size="7"><B>Get the<Br>
+				 	Wedding<BR>Survival Kit<BR>
+				 fast -- in 3<BR>easy steps!</font></td></B>
+		</tr></table>
+
+	<table>
+<tr>
+	<TD><img src="Number 1.png" height="100"><BR><BR><BR></td>
+<BR><B><TD><BR><font face="lucida bright" size="5">Do us a favor please. <BR>
+ How did you hear<BR>about us?<br><br></font>
+<input type="text" size="30"><BR><BR>
+<a href="http://aaa.com><font face="lucida bright" size="2">Click to continue - thanks!</B></BR></td></font></a>
+
+
+	<TD><img src="Number 2.png" height="100"><br><BR><BR></TD>
+	<B><BR><BR><BR><TD><font face="lucida bright" size="5">Choose the format<BR>you want below.<BR>
+ <br><br></B><BR></td>
+
+
+	<TD><img src="Number 3.png" height="100"><br><BR><BR></TD>
+	<B><BR><BR><BR><TD><font face="lucida bright" size="5">Click any logo below<Br> for your desired format. <BR>
+ <br><br></B><BR></td>
+</Tr>	</table>
+
+
+
+<BR><BR><HR><BR>
+<font face="lucida bright" size="7">Printed Book<br>
+<font face="lucida bright" size="5">Jumbo Sized - $12.95<br></font><BR>
+
+<a href="http://amazon.com"><img src="Amazon Logo.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+
+<HR><font face="lucida bright" size="7">eBook<br>
+<font face="lucida bright" size="5">Instant Download - $7.95<br></font><BR>
+
+<a href="http://amazon.com"><img src="Amazon Logo.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+<a href="http://bn.com"><img src="Nook.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+<a href="http://apple.com"><img src="Apple Books Logo.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a><BR>
+
+<a href="http://kobo.com"><img src="Kobo.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+<a href="http://scribd.com"><img src="Scribd.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+<a href="http://google.com"><img src="Google Play.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+
+<HR><font face="lucida bright" size="7">Audio Book<br>
+<font face="lucida bright" size="5">Instant Download - $24.95<br></font><BR>
+
+<a href="http://amazon.com"><img src="Amazon Logo.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+<a href="http://nook.com"><img src="Nook.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+<a href="http://apple.com"><img src="Apple Books Logo.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a><BR>
+<a href="http://scribd.com"><img src="Scribd.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+<a href="http://google.com"><img src="Google Play.png" height="100"><font face="lucida bright" size="7" color="white">...</font></a>
+
+
+
+
+
+
+
+
+<BR><BR><BR>
+
+
+<font face="arial" size="2">Entire Contents &copy; 2022 by Brechner, Pitonyak & Schilling | All Rights Reserved </font>
+</body>
+</center>
+</head>
+</html>
